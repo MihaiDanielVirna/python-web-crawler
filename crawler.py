7feb74a6f01b9d5c442847b8f4414e292ff827c6
Mihai-Daniel Virna <virnam@uni.coventry.ac.uk>
2017-04-23 16:05:44 +0100

    CHanged

--- crawler.py
+++ crawler.py
@@ -2,7 +2,6 @@ import queue
 import multiprocessing
 import sys
 import traceback
-import getopt
 
 from threading import Thread
 from urllib import parse
@@ -14,8 +13,10 @@ from collections import defaultdict
 class CustomParser(HTMLParser): #inherit from HTMLParser
     
     #Could have also used BeautifulSoup to find objects with the specified tags
-    def __init__(self, accepted_tag_attribs):
+    def __init__(self, accepted_tag_attribs = ['a href']):
         super(CustomParser, self).__init__()
+        if not accepted_tag_attribs:
+            print('No assets will be saved, just links')
         self.accepted = accepted_tag_attribs
         self.url = None
         self.domain = None
@@ -36,12 +37,12 @@ class CustomParser(HTMLParser): #inherit from HTMLParser
                     if v.startswith('#'):
                         continue
                     #check if relative link
-                    if urlparse(v).netloc and self.domain == urlparse(v).hostname :
-                        self.urls.add(v)
+                    if urlparse(v).netloc and self.domain in urlparse(v).hostname :
+                        self.urls.add(v.rstrip('/'))
                     elif not urlparse(v).netloc \
                             and urlparse(full_url).netloc \
-                            and self.domain == str(urlparse(full_url).hostname):
-                        self.urls.add(full_url)
+                            and self.domain in str(urlparse(full_url).hostname):
+                        self.urls.add(full_url.rstrip('/'))
                     
 class Crawler(object):
     
@@ -53,12 +54,17 @@ class Crawler(object):
         self.accepted = accepted_tag_attrib_pairs
         self.domain =  urlparse(root).hostname
         self.visited = set()
+        self.not_valid_sites = set()
 
-
+    # Create threads equal to cpu count
     def start(self, write_files = True):
         self.create_threads()
-        self.crawl_queue.join()
 
+        #wait for all threads to consume the queue
+        self.crawl_queue.join()
+        
+        self.visited = self.visited.difference(self.not_valid_sites)
+        visited_sorted = sorted(self.visited)
         if write_files:
             assets_file = open('assets.out','w',encoding = 'utf-8', errors = 'replace')
             for page, assets in self.assets.items():
@@ -69,10 +75,10 @@ class Crawler(object):
             assets_file.close()
 
             sitemap = open('sitemap.out','w',encoding = 'utf-8', errors = 'replace')
-            for site in self.visited:
+            for site in visited_sorted:
                 sitemap.write(str(site) + '\n')
             sitemap.close()
-        return 'Finished!'
+        print 'Finished!'
 
     def create_threads(self):
         for i in range(multiprocessing.cpu_count()):
@@ -103,45 +109,11 @@ class Crawler(object):
                 self.visited.update(urls)
                 self.crawl_queue.task_done()
                 parser.reset_data()
+                print(len(self.crawl_queue.queue))
             except:
                 exc_type, exc_value, exc_traceback = sys.exc_info()
                 log_file = open('log.out','a')
                 log_file.write(str(exc_value) + ' When crawling :' + str(item) + '\n')
                 log_file.close()
+                self.not_valid_sites.add(item)
                 self.crawl_queue.task_done()
-                
-if __name__ == '__main__':
-
-    try:
-        options, arguments = getopt.getopt(sys.argv[1:], 'w:v:p', ['website=','valid_assets=', 'profile='])
-        print (options)
-        if not options:
-            raise getopt.GetoptError('Arguments not specified')
-    except getopt.GetoptError as err:
-        print(err)
-        print('\n')
-        print('Example usage : python crawler_multi_thread.py'+\
-              ' --website=http://github.com --valid_assets="a href" -p \n')
-        print('Note : -p is used to profile the code\n')
-        sys.exit(2)
-    
-    import sys
-    import cProfile
-
-    website = ''
-    valid_asset_list = []
-    profile = False
-    for opts, arg in options:
-        if opts in ('-w','--website'):
-            website = arg
-        if opts in ('-v','--valid_assets'):
-            valid_asset_list = arg.split(',')
-        if opts in ('-p','--profile'):
-            profile = True
-    crawler = Crawler((valid_asset_list),website)
-
-    if profile:
-        cProfile.run('crawler.start()')
-    else:
-        crawler.start()
-    
